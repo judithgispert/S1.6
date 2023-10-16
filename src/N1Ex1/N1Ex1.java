c16b3eace3d04bc88d7cdb7db0822ad4f2d73b07
judithgispert <judithgispert@10.63.0.63>
2023-10-16 12:14:53 +0200

    No generic Methods

--- src/N1Ex1/N1Ex1.java
+++ src/N1Ex1/N1Ex1.java
@@ -1,5 +1,12 @@
 package N1Ex1;
 
 public class N1Ex1 {
+	
+	public static void main (String[]args) {
+		
+		NoGenericMethods noGenMeth = new NoGenericMethods("Bed", "Flower", "Dad");
+		NoGenericMethods noGenMeth2 = new NoGenericMethods("Flower", "Dad", "Bed");
+		
+	}
 
 }
